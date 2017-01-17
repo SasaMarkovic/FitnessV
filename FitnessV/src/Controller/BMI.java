bd0464b337c72dede271a7b690b7533220397943
SasaMarkovic <sasa.markovic@protonmail.ch>
2017-01-17 09:50:12 +0100

    BMI - Fertig Kalorienverbraucht - Noch dran User&Daten - Hinzugefügt

--- FitnessV/src/Controller/BMI.java
+++ FitnessV/src/Controller/BMI.java
@@ -1,5 +1,45 @@
 package Controller;
 
 public class BMI {
-
+	
+	private double koerpergewicht;
+	private double koerpergroesse;
+	
+	private double bmi;
+	
+	public BMI(){
+		
+	}
+	
+	public void berechneBMIFrau(){
+		this.bmi = koerpergewicht / (koerpergroesse * koerpergroesse);
+	}
+	
+	public void berechneBMIMann(){
+		this.bmi = koerpergewicht / (koerpergroesse * koerpergroesse);
+	}
+	
+	public double getbmi(){
+		return this.bmi;
+	}
+	
+	public double getkoerpergewicht(){
+		return this.koerpergewicht;
+	}
+	
+	public double getkoerpergroesse(){
+		return this.koerpergroesse;
+	}
+	
+	public void setbmi(double args){
+		this.bmi = args;
+	}
+	
+	public void setkoerpergewicht(double args){
+		this.koerpergewicht = args;
+	}
+	
+	public void setkoerpergroesse(double args){
+		this.koerpergroesse = args;
+	}
 }
--- FitnessV/src/Controller/Kalorienverbrauch.java
+++ FitnessV/src/Controller/Kalorienverbrauch.java
@@ -1,5 +1,42 @@
 package Controller;
-
+//http://www.sportunterricht.ch/Theorie/Energie/energie.php
 public class Kalorienverbrauch {
-
+	
+	//kcal/24h
+	private double grundumsatz;
+	private double leistungsumsatz;
+	
+	private String geschlecht;
+	//in cm
+	private double groesse;
+	//in kg
+	private double gewicht;
+	private int alter;
+	
+	
+	//PAL WErte
+	private double sitzend_liegend;
+	private double sitzend_kaum;
+	private double sitzend_gehend_stehend;
+	private double stehend_gehend;
+	private double anstrengende_arbeit;
+	
+	public Kalorienverbrauch(){
+		sitzend_liegend = 1.2;
+		sitzend_kaum = 1.45;
+		sitzend_gehend_stehend = 1.65;
+		stehend_gehend = 1.85;
+	}
+	
+	public void berechnegrundumsatzMann(){
+		this.grundumsatz = 66.47 + (13.7 * this.gewicht) + (5 * this.groesse) - (6.8 * alter);
+	}
+	
+	public void berechnegrundumsatzFrau(){
+		
+	}
+	
+	public void berechneleistungsumsatz(){
+		
+	}
 }
