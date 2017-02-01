a0c3d55eb5d12472b579bc1f8842ff9085d2ee31
SasaMarkovic <sasa.markovic@protonmail.ch>
2017-02-02 00:16:54 +0100

    New Login-Frame created

--- FitnessV/src/Start.java
+++ FitnessV/src/Start.java
@@ -1,6 +1,7 @@
 
+import View.*;
+
 public class Start {
 	public static void main(String[] args){
-		new Controller.Controller();
 	}
 }
--- FitnessV/src/View/Controller.java
+++ FitnessV/src/View/Controller.java
@@ -1,9 +1,19 @@
 package View;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+
 /**
  * Handled das Package
  * @author Sasa
  *
  */
 public class Controller {
-
+	Login logframe;
+	public Controller(){
+		logframe = new Login();
+	}
+	public void displayLogin(){
+		logframe.launchFrame();
+	}
 }
